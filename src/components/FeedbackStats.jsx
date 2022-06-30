1e6006ae1c63370d5b0573127c59c35a9ce1f1c8
George Escobar <geo.escobar214@gmail.com>
2022-06-29 21:35:15 -0500

    feedback stats added

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,12 +1,24 @@
+import PropTypes from "prop-types";
 
+function FeedbackStats({ feedback }) {
+  // calculate ratings average
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
 
-function FeedbackStats({feedback}) {
+  average = average.toFixed(1);
+
   return (
     <div className="feedback-stats">
-        <h4>{feedback.length} Reviews</h4>
-        <h4>Average Rating: 10</h4>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
-  )
+  );
 }
 
-export default FeedbackStats
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbackStats;
